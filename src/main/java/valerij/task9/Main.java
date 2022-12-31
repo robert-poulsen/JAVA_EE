00f75af270fbcfda8568195c252863041eec3c39
robert-poulsen <dagovecvalerij@gmail.com>
2023-01-01 01:57:11 +0200

    Lab9

---
+++ src/main/java/valerij/task9/Main.java
@@ -0,0 +1,13 @@
+package valerij.task9;
+
+public class Main {
+    public static void main(String[] args) {
+        final Bank bank = new Bank();
+
+        Thread1 thread1 = new Thread1(bank);
+        Thread2 thread2 = new Thread2(bank);
+
+        thread1.start();
+        thread2.start();
+    }
+}
